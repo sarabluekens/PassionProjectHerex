3ab4e28fa8f063db4ebacfa036999d050abe8eea
sarabluekens <sara.bluekens@skynet.be>
2021-08-18 18:08:07 +0200

    first unity testing en tf testing
    first efforts of getting to know unity en testing by building a tutorial game and also try to get tensorflow working with babylon

---
+++ TfTest copy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+  private float speed = 15.0f;
+  private float turnSpeed = 50;
+  private float horizontalInput;
+  private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame 
+    void Update()
+    {
+      // get player input
+      horizontalInput = Input.GetAxis("Horizontal");
+      forwardInput = Input.GetAxis("Vertical");
+
+        // move forward
+      transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput); 
+
+      // rotate vehicle
+      transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+
+
+    }
+}
---
+++ sneakysam1 copy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 direction;
+
+    //actieve baan baan  L = 0, M = 1, R = 2
+    private int runlane = 1;
+
+    // afstand tussen 2 banen
+    private float laneDistance = 4;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        controller.Move(direction * Time.deltaTime);
+
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            runlane++;
+            if(runlane == 3)
+                runlane = 2;
+            
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            runlane--;
+            if (runlane == -1)
+                runlane = 0;
+
+        }
+
+        Vector3 targetPostition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if(runlane == 0)
+        {
+            targetPostition += Vector3.left * laneDistance;
+        }else if (runlane == 2)
+        {
+            targetPostition += Vector3.right * laneDistance;
+
+        }
+        transform.position = targetPostition;
+    }
+}
