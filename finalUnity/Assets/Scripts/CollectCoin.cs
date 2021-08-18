d2a6a5f8856d808a58df4d053d495c5f99a2c3dc
sarabluekens <sara.bluekens@skynet.be>
2021-08-18 18:21:30 +0200

    final unity2
    dit is het finale unity project met volledig werkende code (deze is het echt)

---
+++ finalUnity/Assets/Scripts/CollectCoin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoin : MonoBehaviour
+{
+    public AudioSource collectSound;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<AudioManager>().playSound("coin");
+        ScoringSystem.theScore += 10;
+        Destroy(gameObject);
+    }
+}
