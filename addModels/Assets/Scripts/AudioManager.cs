0f3ae79a036948bf0823ea39d2536997cf0874c8
sarabluekens <sara.bluekens@skynet.be>
2021-08-18 18:00:20 +0200

    models added
    addding models and textures to the game, and background

---
+++ addModels/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+        }
+    }
+
+    // Update is called once per frame
+    public void playSound(string name)
+    {
+        foreach (Sound sound in sounds)
+        {
+            if (sound.name == name)
+                sound.source.Play();
+        }
+    }
+}
---
+++ addModels/Assets/Scripts/TileManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+
+    public float zSpawn = 0; // z variabele waar de tiles moeten spawnen (x en y veranderen niet)
+
+    public float tileLength = 30; // is het wel dertig?
+    public int numberOfTiles = 5;
+
+    public Transform playerTransform;
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        //spawnTile(0);
+        //spawnTile(1);
+        //spawnTile(4);
+
+        for(int i = 0; i < numberOfTiles; i++)
+        {
+            if (i == 0)
+            {
+                SpawnTile(0);
+            }else
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerTransform.position.z - 35 > zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            Delete();
+        }
+        
+    }
+
+    public void SpawnTile(int tileIndex)
+    {
+        GameObject tile = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(tile);
+        zSpawn += tileLength; //zorgen dat ze tegen elkaar spawnen
+    }
+
+    private void Delete()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
