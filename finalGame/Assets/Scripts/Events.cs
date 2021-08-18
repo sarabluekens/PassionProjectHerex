6aa5ce4cd935c71d2fb0407c4d36a5a42367d8ae
sarabluekens <sara.bluekens@skynet.be>
2021-08-18 18:11:33 +0200

    final working project
    the project with the final code in unity

---
+++ finalGame/Assets/Scripts/Events.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Events : MonoBehaviour
+{
+
+ public void ReplayGame()
+    {
+        SceneManager.LoadScene("Game");
+        ScoringSystem.theScore = 0;
+    }
+
+public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
