108ade60144a6848c9453c33502370b7cbc962ae
sarabluekens <sara.bluekens@skynet.be>
2021-08-18 17:48:53 +0200

    evolutie van de runnergame in Unity
    evolution of the runner game in unity

---
+++ sneakysam3/Assets/Scripts/TileManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+
+    public float zSpawn = 0; // z variabele waar de tiles moeten spawnen (x en y veranderen niet)
+
+    public float tileLength = 30; // is het wel dertig?
+    public int numberOfTiles = 5;
+
+    public Transform playerTransform;
+
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        //spawnTile(0);
+        //spawnTile(1);
+        //spawnTile(4);
+
+        for(int i = 0; i < numberOfTiles; i++)
+        {
+            if (i == 0)
+            {
+                SpawnTile(0);
+            }else
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerTransform.position.z > zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
+        
+    }
+
+    public void SpawnTile(int tileIndex)
+    {
+        Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        zSpawn += tileLength; //zorgen dat ze tegen elkaar spawnen
+    }
+}
---
+++ sneakysam5/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    //
+    public Transform player;
+
+    //offset tussen player en camera
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //berekent de positie voor de camera adhv de positie van de player
+        Vector3 cameraPosition = new Vector3(transform.position.x, transform.position.y, offset.z + player.position.z);
+        transform.position = Vector3.Lerp(transform.position, cameraPosition, 10 *Time.deltaTime);
+    }
+}
